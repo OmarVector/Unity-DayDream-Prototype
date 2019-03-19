272f11a58f93c12a0e212d5ff877a4adb4589bff
Omar <omar.ahmed.vc@gmail.com>
2019-03-19 08:55:52 +0100

    VR 08: UI Design
    -Adding : UI Theme Assets
    - Adding : UITheme ScriptableObject to assign player HUD theme , and UISetter scripter
    -Updating gitIgnore

---
+++ Assets/Scripts/UI/UISetter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UISetter : MonoBehaviour
+{
+   [SerializeField] private UITheme Theme;
+  
+   [SerializeField] private Image crossHair;
+   [SerializeField] private Image healthBackgroundImage;
+   [SerializeField] private Image healthFillImage;
+   [SerializeField] private Image armorFillImage;
+ 
+   [SerializeField] private Image ammoBackgroundImage;
+   [SerializeField] private Image rightWeaponAmmo;
+   [SerializeField] private Image leftWeaponAmmo;
+
+   private void Awake()
+   {
+      crossHair.sprite = Theme.CrossHair;
+      healthBackgroundImage.sprite = Theme.HealthBackgroundImage;
+      healthFillImage.sprite = Theme.HealthFillImage;
+      armorFillImage.sprite = Theme.ArmorFillImage;
+
+      ammoBackgroundImage.sprite = Theme.AmmoBackgroundImage;
+      rightWeaponAmmo.sprite = Theme.RightWeaponAmmo;
+      leftWeaponAmmo.sprite = Theme.LeftWeaponAmmo;
+   }
+}
---
+++ Assets/Scripts/UI/UITheme.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "UI Theme/Player UI")]
+public class UITheme : ScriptableObject
+{
+    [Header("Player CrossHair")]
+    public Sprite CrossHair;
+   
+    [Header("Player State UI")]
+    public Sprite HealthBackgroundImage;
+    public Sprite HealthFillImage;
+    public Sprite ArmorFillImage;
+
+    [Header("Weapon State UI")] 
+    public Sprite AmmoBackgroundImage;
+    public Sprite RightWeaponAmmo;
+    public Sprite LeftWeaponAmmo;
+
+}
